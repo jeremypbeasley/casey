614bf5b3a3f5da62cf0016361cacdd9921be21fa
J. Beasley <jeremypbeasley@jbeasley007554.starbucks.net>
2016-07-21 02:16:46 -0700

    cleaned up home and got basic showw and hide done for tab nav

--- public/master.js
+++ public/master.js
@@ -10036,5 +10036,27 @@ if ( !noGlobal ) {
 return jQuery;
 } ) );
 
+$(document).ready(function() {
+
+	$('.ContentContainer.Home').show();
+	$('.ContentContainer.Home').addClass("Active");
+
+function renderSection(section) {
+	$('.ContentContainer').hide();
+	var newsection = ".ContentContainer." + section;
+	console.log(newsection);
+	$(newsection).show();
+};
+
+$(".AppNav li").click(function() {
+  var thesection = $(this).data('section');
+	renderSection(thesection);
+});
+
+});
+
+
+
+
 
 
--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -1,2 +1,24 @@
+$(document).ready(function() {
+
+	$('.ContentContainer.Home').show();
+	$('.ContentContainer.Home').addClass("Active");
+
+function renderSection(section) {
+	$('.ContentContainer').hide();
+	var newsection = ".ContentContainer." + section;
+	console.log(newsection);
+	$(newsection).show();
+};
+
+$(".AppNav li").click(function() {
+  var thesection = $(this).data('section');
+	renderSection(thesection);
+});
+
+});
+
+
+
+
 
 
