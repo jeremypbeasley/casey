ddd5d899446adbdf5719010dfa4b0dcd8932e7d8
J. Beasley <jeremypbeasley@jbeasley007554.starbucks.net>
2017-01-18 20:37:02 -0800

    stable transaction detail overlay

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
 var db
 
-
-
-// MONGO
+// DATA
 
 const MongoClient = require('mongodb').MongoClient
 MongoClient.connect('mongodb://admin:almighty@ds035348.mlab.com:35348/caseyappv1', (err, database) => {
@@ -17,80 +14,73 @@ MongoClient.connect('mongodb://admin:almighty@ds035348.mlab.com:35348/caseyappv1
   })
 })
 
+// SETUP
+
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
-app.use('jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
-
 
 // ROUTING
 
-// app.get('/', (req, res) => {
-//   db.collection('transactions').find().toArray((err, result) => {
-//     res.render('pages/index.ejs');
-//   })
-// })
+app.get('/api/transactions/:id', (req, res) => {
+  db.collection('transactions')
+    .find({ _id: req.params.id })
+    .toArray((err, result) => {
+      if (err) { return console.log(err); }
 
-// app.get('/transactions', (req, res) => {
-//   db.collection('transactions').find().toArray((err, result) => {
-//     if (err) return console.log(err)
-//     res.render('pages/transactions.ejs', {transactions: result})
-//   })
-// })
-
-app.get('/', (req, res) => {
-  db.collection('transactions').find().toArray((err, result) => {
-    if (err) return console.log(err)
-    res.render('pages/index.ejs', {transactions: result})
-  });
-  var yikes = db.collection('transactions');
-  console.log(yikes);
+      if (!result.length) { 
+        return console.log(err); 
+      } else {
+        res.send(result[0]);
+      }
+    });
 });
 
+app.get('/api/transactions', (req, res) => {
+  db.collection('transactions')
+    .find()
+    .toArray((err, result) => {
+      if (err) { return console.log(err); }
+      res.send(result);
+    });
+});
+
+app.get('/*', (req, res) => {
+  db.collection('transactions').find().toArray((err, result) => {
+    if (err) { return console.log(err); }
+    res.render('pages/index.ejs', {transactions: result})
+  }); 
+});
+
+// fetch(`/transactions/${transactionId}`)
+//   .then(renderModal)
+
+// function thisisathing(Vendor) {
+//   db.collection('transactions').findOne({ 'name': Vendor }, function (err, person) {
+//     if (err) return handleError(err);
+//     console.log("This purchase total was $" + person.amount + ".");
+//     return person.amount;
+//   })
+// }
+// thisisathing("iTunes");
 
 
-
-
-// app.get('/transaction_detail', (req, res) => {
-//     res.render('modals/transaction_detail.ejs')
+// app.get('/budget_detail', (req, res) => {
+//     res.render('modals/budget_detail.ejs')
 // })
 
-// app.get('/transaction_edit', (req, res) => {
-//     res.render('modals/transaction_edit.ejs')
+// app.get('/budget_edit', (req, res) => {
+//     res.render('modals/budget_edit.ejs')
 // })
 
-app.get('/budget_detail', (req, res) => {
-    res.render('modals/budget_detail.ejs')
-})
-
-app.get('/budget_edit', (req, res) => {
-    res.render('modals/budget_edit.ejs')
-})
-
-app.get('/account_personalinfo_details', (req, res) => {
-    res.render('modals/account_personalinfo_details.ejs')
-})
-
-app.get('/account_personalinfo_edit', (req, res) => {
-  res.render('modals/account_personalinfo_edit.ejs')
-}) 
-
-app.get('/login', (req, res) => {
-    res.render('pages/login.ejs')
-})
-
-app.get('/login_form', (req, res) => {
-    res.render('pages/login_form.ejs')
-})
-
-// app.get('/bills', (req, res) => {
-//     res.render('pages/bills.ejs')
+// app.get('/account_personalinfo_details', (req, res) => {
+//     res.render('modals/account_personalinfo_details.ejs')
 // })
 
-// app.get('/account', (req, res) => {
-//     res.render('pages/account.ejs')
-// })
+// app.get('/account_personalinfo_edit', (req, res) => {
+//   res.render('modals/account_personalinfo_edit.ejs')
+// }) 
 
 // app.get('/login', (req, res) => {
 //     res.render('pages/login.ejs')
@@ -99,95 +89,4 @@ app.get('/login_form', (req, res) => {
 // app.get('/login_form', (req, res) => {
 //     res.render('pages/login_form.ejs')
 // })
-
-// ADDING NEW ITEMS
-
-app.use(bodyParser.urlencoded({extended: true}))
-
-app.post('/transactions', (req, res) => {
-  db.collection('transactions').save(req.body, (err, result) => {
-    if (err) return console.log(err)
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
-
-// UPDATING ITEMS
-
-app.put('/transactions', (req, res) => {
-  db.collection('transactions')
-  .findOneAndUpdate({category: 'France'}, {
-    $set: {
-      amount: req.body.amount,
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
-})
-
-app.delete('/transactions', (req, res) => {
-  db.collection('transactions').findOneAndDelete({category: req.body.category}, (err, result) => {
-    if (err) return res.send(500, err)
-    res.send('A darth vadar quote got deleted')
-  })
-})
-
-// LEDGER
-
-
-
-// app.put('/transactions', (req, res) => {
-//   db.collection('transactions')
-//   .findOneAndUpdate({category: 'France'}, {
-//     $set: {
-//       amount: req.body.amount,
-//     }
-//   }, {
-//     sort: {_id: -1},
-//     upsert: true
-//   }, (err, result) => {
-//     if (err) return res.send(err)
-//     res.send(result)
-//   })
-// })
-
-
-// PLAID
-
-// var plaid = require('plaid');
-// var access_token = "";
-
-// var plaidClient = new plaid.Client('576cd20566710877408d0572'
-// , '945fb4239cb1de171fd6feb4e41291', plaid.environments.tartan);
-
-// plaidClient.addAuthUser('wells', {
-//   username: 'XXXXXXXXX',
-//   password: 'XXXXXXXX',
-// }, function(err, mfaResponse, response) {
-//   if (err != null) {
-//     console.error(err);
-//   } else if (mfaResponse != null) {
-//     plaidClient.stepAuthUser(mfaResponse.access_token, 'tomato', {},
-//     function(err, mfaRes, response) {
-//       console.log(response.accounts);
-//     });
-//   } else {
-//     console.log(response.accounts);
-//     var access_token = response.access_token;
-//   }
-// });
-
-// plaidClient.getConnectUser(access_token, {
-//   gte: '30 days ago',
-// }, function(err, response) {
-//   console.log(access_token)
-// });
-
-
-
-
 
